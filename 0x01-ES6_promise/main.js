dccdaf8a65f286a0fd0f3ae00aaeba9f7c9ab6fe
David <obododavidsop@gmail.com>
2024-04-26 21:11:38 +0100

    100

--- 0x01-ES6_promise/main.js
+++ 0x01-ES6_promise/main.js
@@ -1,23 +1,8 @@
-import loadBalancer from "./7-load_balancer";
-
-const ukSuccess = 'Downloading from UK is faster';
-const frSuccess = 'Downloading from FR is faster';
-
-const promiseUK = new Promise(function(resolve, reject) {
-    setTimeout(resolve, 100, ukSuccess);
-});
-
-const promiseUKSlow = new Promise(function(resolve, reject) {
-    setTimeout(resolve, 400, ukSuccess);
-});
-
-const promiseFR = new Promise(function(resolve, reject) {
-    setTimeout(resolve, 200, frSuccess);
-});
+import asyncUploadUser from "./100-await";
 
 const test = async () => {
-    console.log(await loadBalancer(promiseUK, promiseFR));
-    console.log(await loadBalancer(promiseUKSlow, promiseFR));
-}
+    const value = await asyncUploadUser();
+    console.log(value);
+};
 
 test();
