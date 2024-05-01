bf2f002068959f9a56f714fba8ecdfd628d5d6c9
David <obododavidsop@gmail.com>
2024-05-01 05:52:05 +0100

    10-update_uniq_items.js a

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,4 +1,8 @@
 export default function updateUniqueItems(groceries) {
+  if (!(groceries instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+
   groceries.forEach((val, key) => {
     if (val === 1) {
       try {
