45d264b9fa9f7d409e09dc00ca83394913e26d66
David <obododavidsop@gmail.com>
2024-05-01 20:20:34 +0100

    unnecessary update 100

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,9 +1,11 @@
 export const weakMap = new WeakMap();
 
 export function queryAPI(endpointArg) {
-  const queries = weakMap.has(endpointArg) ? weakMap.get(endpointArg) : 0;
-  weakMap.set(endpointArg, queries + 1);
-  if (queries >= 5) {
+  if (!weakMap.has(endpointArg)) {
+    weakMap.set(endpointArg, 0);
+  }
+  weakMap.set(endpointArg, weakMap.get(endpointArg) + 1);
+  if (weakMap.get(endpointArg) >= 5) {
     throw new Error('Endpoint load is high');
   }
 }
