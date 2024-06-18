62a7c26eb30462e9adf21460cbd66952494df22d
David <obododavidsop@gmail.com>
2024-06-18 10:28:37 +0100

    2-read_file.js

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  // read database synchronously
+  try {
+    const db = fs.readFileSync(path, { encoding: 'utf-8', flag: 'r' });
+
+    const lines = db.split('\n').filter((line) => line.trim() !== '');
+    console.log(`Number of students: ${lines.length - 1}`);
+
+    const fields = {};
+    for (let i = 1; i < lines.length; i += 1) {
+      const line = lines[i].split(',');
+      if (!fields[line[3]]) {
+        fields[line[3]] = [line[0]];
+      } else {
+        fields[line[3]].push(line[1]);
+      }
+    }
+
+    for (const [field, names] of Object.entries(fields)) {
+      console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
+    }
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
