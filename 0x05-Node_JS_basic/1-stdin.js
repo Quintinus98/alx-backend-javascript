c69e2b7f20b8f81ef12113cec1f6e4050f834fa6
David <obododavidsop@gmail.com>
2024-06-18 05:23:29 +0100

    1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -7,5 +7,8 @@ const rl = readline.createInterface({
 
 rl.question('Welcome to Holberton School, what is your name?\n', (answer) => {
   console.log(`Your name is: ${answer}`);
+  if (!process.stdin.isTTY) {
+    console.log('This important software is now closing');
+  }
   rl.close();
 });
