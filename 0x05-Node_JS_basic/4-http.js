4313cabcf48ad4d4dbe9c3de1e0aa3ed34b0c12d
David <obododavidsop@gmail.com>
2024-07-27 07:19:54 +0100

    update 4

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -3,12 +3,15 @@ const http = require('http');
 const host = 'localhost';
 const port = 1245;
 
-const requestListener = (req, res) => {
-  res.writeHead(200);
-  res.end('Hello Holberton School!');
-};
+const app = http.createServer((req, res) => {
+  const body = 'Hello Holberton School!';
+  res.writeHead(200, {
+    'Content-Length': Buffer.byteLength(body),
+    'Content-Type': 'text/plain',
+  });
+  res.write(Buffer.from(body));
+});
 
-const app = http.createServer(requestListener);
 app.listen(port, host, () => {
   console.log(`Server is running on http://${host}:${port}`);
 });
